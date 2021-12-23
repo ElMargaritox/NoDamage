9a63dae2ff1d1960af3eaeb0b6bbfb38fe447737
elmargaritox <alexisguzman0303@gmail.com>
2021-12-22 23:32:30 -0300

    Agregar archivos de proyecto.

---
+++ NoDamage/Configuration/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoDamage.Configuration
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public bool BlockDamageGuns;
+        public bool BlockDamageMelee;
+        public bool BlockDamagePunch;
+        public bool BlockDamageVehicle;
+        public bool BlockDamageZombie;
+        public void LoadDefaults()
+        {
+            BlockDamageGuns = false;
+            BlockDamageMelee = false;
+            BlockDamagePunch = true;
+            BlockDamageVehicle = false;
+            BlockDamageZombie = true;
+        }
+    }
+}
---
+++ NoDamage/NoDamagePlugin.cs
@@ -0,0 +1,41 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned.Events;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace NoDamage
+{
+    public class NoDamagePlugin : RocketPlugin<Configuration.Configuration>
+    {
+        public static NoDamagePlugin Instance { get; set; }
+
+        protected override void Load()
+        {
+            Logger.Log("Plugin By Margarita#8172");
+            DamageTool.damagePlayerRequested += DamageTool_damagePlayerRequested;
+        }
+
+        private void DamageTool_damagePlayerRequested(ref DamagePlayerParameters parameters, ref bool shouldAllow)
+        {
+            if (parameters.cause == EDeathCause.PUNCH & Configuration.Instance.BlockDamagePunch) shouldAllow = false;
+            if (parameters.cause == EDeathCause.GUN & Configuration.Instance.BlockDamageGuns) shouldAllow = false;
+            if (parameters.cause == EDeathCause.VEHICLE & Configuration.Instance.BlockDamageVehicle) shouldAllow = false;
+            if (parameters.cause == EDeathCause.MELEE & Configuration.Instance.BlockDamageMelee) shouldAllow = false;
+            if (parameters.cause == EDeathCause.ZOMBIE & Configuration.Instance.BlockDamageZombie) shouldAllow = false;
+        }
+
+
+
+        protected override void Unload()
+        {
+            DamageTool.damagePlayerRequested -= DamageTool_damagePlayerRequested;
+        }
+    }
+}
